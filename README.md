65120ca06a1e2ac48a5350b2c7fbe9b2330e2f5d
konrathj <konrathj@student.ethz.ch>
2021-05-17 14:28:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# QuickSetup
+
+Script to easily set up my default working environment on a new server
+
+
+
+- Clones [Nighmared/dotfiles](https://github.com/Nighmared/dotfiles) repo and sets up config for
+  - zsh
+  - htop
+  - neofetch
+  - nano
+- Installs oh-my-zsh and configures themes and plugins
+
+
+
+
+
+TODO:
+
+- [ ] Add fetching authorized ssh keys from somewhere
---
+++ quicksetup.sh
@@ -0,0 +1,36 @@
+echo "might ask for sudo if not run as root because of apt\n also if not run as root and sudo not yet installed, this wont work :> \n"
+
+prog=1
+
+echo "$prog/?: cloning config files"
+git clone -b lightweight https://github.com/Nighmared/dotfiles ~/.config #get some configs
+
+prog=$((prog+1))
+echo "$prog/?: installing stuff"
+sudo apt install sudo htop neofetch wget git zsh python3 python3-pip python3-venv command-not-found -y
+prog=$((prog+1))
+echo "$prog/?: setting zsh as shell"
+chsh -s /bin/zsh
+prog=$((prog+1))
+echo "$prog/?: installing oh-my-zsh"
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"		#install oh-my-zsh
+prog=$((prog+1))
+
+echo "$prog.1/? add pip install location to path"
+echo "export PATH=\"$HOME/.local/bin:\$PATH\"">>~/.config/zsh/.zshrc
+echo "$prog.2/? installing zsh plugins "
+qsetup_orig=$(pwd) #go back to where it was :>
+cd ~/.config
+git submodule update --init --recursive #for plugins :facepalm:
+cd $qsetup_orig
+unset qsetup_orig
+
+pip3 install pygments
+prog=$((prog+1))
+
+echo "$prog/?: symlink zshrc file -.-"
+rm ~/.zshrc
+ln -s ~/.config/zsh/.zshrc ~/.zshrc
+prog=$((prog+1))
+
+echo "---------------done--------------"
